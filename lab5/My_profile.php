8d39a3e0c1c82bd01dfef3eaed099fab009e4f4d
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-08-02 09:33:28 +0600

    1

---
+++ lab5/My_profile.php
@@ -0,0 +1,12 @@
+<html>
+     <head></head>
+     <?php $name = "Mutasim";?>
+ <body>
+     <h1> align="center">My profile</h1>
+     <table align="center">
+         <tr>
+             <td><?php echo $name;?></td>
+         </tr>
+     </table>
+ </body>
+</html>
