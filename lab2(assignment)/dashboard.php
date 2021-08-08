72e2d633dabeaddbff974ad8c7714af66ab3b953
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-08-08 22:04:00 +0600

    1

---
+++ lab2(assignment)/dashboard.php
@@ -0,0 +1,26 @@
+<?php include 'main_header.php';?>
+	<h3 >Recent Sales</h3>
+	<table>
+		<thead>
+			<th>Sl#</th>
+			<th>Product Name</th>
+			<th>Product Price</th>
+			<th>Sales Qty</th>
+			<th>Sales Date</th>
+		</thead>
+		<tbody>
+			<td>1</td>
+			<td>Potato</td>
+			<td>60</td>
+			<td>10</td>
+			<td>13.7.2021</td>
+		</tbody>
+        <tbody>
+			<td>2</td>
+			<td>Chicken</td>
+			<td>300</td>
+			<td>20</td>
+			<td>13.7.2021</td>
+		</tbody>
+	</table>
+	<?php include 'main_footer.php';?>
---
+++ lab2(assignment)/main_footer.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<footer class="footer">
+            <h4>Webtech sample Project</h4>
+        </footer>
+
+    </body>
+</html>
